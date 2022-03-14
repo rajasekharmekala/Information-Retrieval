96867e69eab7e1f9f6cfdf3f128a41cb6d548edd
AGNI RAJ BAIKANI <agnirajbaikani@AGNIs-MacBook-Pro.local>
2022-03-14 11:27:01 -0700

    web changes

--- As3/web_search.py
+++ As3/web_search.py
@@ -1,5 +1,6 @@
 # importing Flask and other modules
-from flask import Flask, request, json
+from curses import flash
+from flask import Flask, request, json, render_template
 import numpy as np
 from search import Search
 
@@ -13,16 +14,23 @@ def search(text):
     data = {}
     data["result"] = []
     if request.method == "GET":
-        # results = Search().search_query(text,5)
+        results = Search().search_query(text,5)
+        data['result'] = results
         # columns = [0, 1, 2, 3, 4]
-        data['result'] =  ["https://canvas.eee.uci.edu/courses/43306", "https://edstem.org/us/courses/16359/discussion/1251389", "https://www.geeksforgeeks.org/how-to-create-dictionary-and-add-key-value-pairs-dynamically/"
-                   , "https://www.igb.uci.edu/~pfbaldi/", "https://panageas.github.io/"]
+        # data['result'] =  ["https://canvas.eee.uci.edu/courses/43306", "https://edstem.org/us/courses/16359/discussion/1251389", "https://www.geeksforgeeks.org/how-to-create-dictionary-and-add-key-value-pairs-dynamically/"
+        #            , "https://www.igb.uci.edu/~pfbaldi/", "https://panageas.github.io/"]
     response = app.response_class(
         response=json.dumps(data),
         status=200,
         mimetype='application/json'
     )
+
     return response
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
 
 
 if __name__ == '__main__':
